9b51ccd4c0077d6a54eca7ad674332b435f1762a
Chethila Mallawaarachchi <lavan.20200534@iit.ac.lk>
2022-01-07 22:41:22 +0530

    Hello World project commit

---
+++ src/com/codewithmosh/Main.java
@@ -0,0 +1,13 @@
+package com.codewithmosh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(calc(2,2));
+    }
+    public static float calc(float a, float b) {
+        float total = a + b ;
+        return total;
+    }
+}
+
